0ff8bdb9f3e0d2d5b904a7569a8c8b90f549a247
geasseur <baptiste.ventura@neuf.fr>
2017-09-27 16:08:40 +0200

    form for created a new site

--- vue/vueindex.php
+++ vue/vueindex.php
@@ -21,6 +21,58 @@
           ?>
           <header>
             <h1>liste Chantier</h1>
+            <form id="nouveauChantier" action="index.php" method="post">
+              <label for="">nom</label>
+              <input type="text" name="nom" value=""><br>
+              <label for="">responsable</label>
+              <input type="text" name="responsable" value=""><br>
+              <label for="">duree</label><br>
+              <select name="jour">
+                <option value="1jour">un jour</option>
+                <option value="2jour">deux jour</option>
+                <option value="3jour">trois jour</option>
+                <option value="4jour">quatre jour</option>
+                <option value="5jour">cinq jour</option>
+                <option value="6jour">six jour</option>
+                <option value="7jour">sept jour</option>
+              </select>
+              <select name="semaine">
+                <option value="1semaine">une semaine</option>
+                <option value="2semaine">deux semaines</option>
+                <option value="3semaine">trois semaines</option>
+                <option value="4semaine">quatre semaines</option>
+              </select>
+              <select name="mois">
+                <option value="1mois">un mois</option>
+                <option value="2mois">deux mois</option>
+                <option value="3mois">trois mois</option>
+                <option value="4mois">quatre mois</option>
+                <option value="5mois">cinq mois</option>
+                <option value="6mois">six mois</option>
+                <option value="7mois">sept mois</option>
+                <option value="8mois">huit mois</option>
+                <option value="9mois">neuf mois</option>
+                <option value="10mois">dix mois</option>
+                <option value="11mois">onze mois</option>
+                <option value="12 mois">douze mois</option>
+              </select>
+              <select name="annee">
+                <option value="1an">une année</option>
+                <option value="2an">deux années</option>
+                <option value="3an">trois années</option>
+                <option value="4an">quatre années</option>
+                <option value="5an">cinq années</option>
+              </select><br>
+              <label for="">resumé du chantier</label><br>
+              <textarea name="message" rows="8" cols="45">
+              </textarea><br>
+              <label for="">type de chantier</label>
+              <select name="type_chantier">
+                <option value="maison">maison</option>
+                <option value="immeuble">immeuble</option>
+                <option value="usine">usine</option>
+              </select>
+            </form>
           </header>
           <section>
             <h2><?php echo $donnees['nom']; ?></h2>
@@ -29,25 +81,13 @@
             <p><?php echo $donnees['resume']; ?></p>
             <p><?php echo $donnees['responsable']; ?></p>
             <p>Chantier de type: <?php echo $donnees['type_chantier']; ?></p>
+            <form action="control/controlindex.php" method="post">
+              <input style='display:none;' type="id" value="<?php echo $donnees['id'] ?>">
+              <input type="submit" value="Categorie">
+            </form>
           </section>
           <?php
         }?>
-        <form class="" action="index.php" method="post">
-          <label for="">nom</label>
-          <input type="text" name="nom" value=""><br>
-          <label for="">responsable</label>
-          <input type="text" name="responsable" value=""><br>
-          <label for="">duree</label>
-          <select name="jour">
-            <option value="un jour">un jour</option>
-          </select>
-          <select name="semaine">
-
-          </select>
-          <select name="mois">
-
-          </select>
-        </form>
       </main>
 
         <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
